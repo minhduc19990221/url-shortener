dba2aa3fed0e78b25735e2d010c0155d2cd5cc3c
--local <duc.nguyencaominh@neopay.vn>
2022-12-30 09:00:12 +0700

    Add redirect view url

--- src/main/java/com/minhduc/urlshortener/controller/URLController.java
+++ src/main/java/com/minhduc/urlshortener/controller/URLController.java
@@ -1,17 +1,20 @@
 package com.minhduc.urlshortener.controller;
+// TODO: https://medium.com/hackernoon/url-shortening-service-in-java-spring-boot-and-redis-d2a0f8848a1d
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.minhduc.urlshortener.service.URLConverterService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.validation.Valid;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 
 @RestController
@@ -24,9 +27,24 @@ public class URLController {
     }
 
     @RequestMapping(value = "/shortener", method = RequestMethod.POST, consumes = {"application/json"})
-    public String shortenUrl(@RequestBody @Valid final ShortenRequest shortenRequest) {
-        LOGGER.info("");
-        return "TODO: https://medium.com/hackernoon/url-shortening-service-in-java-spring-boot-and-redis-d2a0f8848a1d";
+    public String shortenUrl(@RequestBody @Valid final ShortenRequest shortenRequest, HttpServletRequest request) throws Exception {
+          String longUrl = shortenRequest.getUrl();
+          LOGGER.info("Received url to shorten: " + longUrl);
+          String localURL = request.getRequestURL().toString();
+          String shortenedURL = urlConverterService.shortenURL(localURL, longUrl);
+          LOGGER.info("Shortened url to: " + shortenedURL);
+          return shortenedURL;
+          // throw new Exception("Please enter a valid URL");
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public RedirectView redirectUrl(@PathVariable String id, HttpServletRequest request, HttpServletResponse response) throws
+            IOException, URISyntaxException, Exception {
+        LOGGER.debug("Received shortened url to redirect: " + id);
+        String redirectUrlString = urlConverterService.getLongURLFromID(id);
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl("http://" + redirectUrlString);
+        return redirectView;
     }
 }
 class ShortenRequest{
